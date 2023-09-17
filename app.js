7638d8948d26806796502fa319fcb07fadae0952
Prateek <sharmaprateek499@gmail.com>
2023-09-17 20:13:16 +0530

    functionality added using mySQL

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
@@ -18,7 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-//Page not found
 app.use(errorController.get404);
 
 app.listen(3000);
